8798b224b3bb0c21beb053178050c6c03be46fa1
Fedor <fedor.kis@gmail.com>
2020-01-20 13:08:26 +0100

    Add lowcase characters

--- rotcipher.rb
+++ rotcipher.rb
@@ -1,10 +1,10 @@
 class Rot_Cipher
 def cipher (string, shift)
-  alphabet = Array('A'..'Z')
+  alphabet = Array('A'..'Z') + Array('a'..'z')
   rotate = Hash[alphabet.zip(alphabet.rotate(shift))]
   string.chars.map{ |c| rotate.fetch(c, " ")}
 end
 end
 
 test = Rot_Cipher.new
-puts test.cipher("CIPHER", 1)
+puts test.cipher("CIPHeR", 1)
