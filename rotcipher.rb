3fe84c883967ff698091e05cbb9be91d7d26f473
Fedor <fedor.kis@gmail.com>
2020-01-20 15:06:57 +0100

    Add text in one line

--- rotcipher.rb
+++ rotcipher.rb
@@ -2,9 +2,9 @@ class Rot_Cipher
 def cipher (string, shift)
   alphabet = Array('A'..'Z') + Array('a'..'z')
   rotate = Hash[alphabet.zip(alphabet.rotate(shift))]
-  string.chars.map{ |c| rotate.fetch(c, " ")}
+  string.chars.map{ |c| rotate.fetch(c)}
 end
 end
 
 test = Rot_Cipher.new
-puts test.cipher("CIPHeR", 1)
+puts test.cipher("CIPHeR", 1).join
