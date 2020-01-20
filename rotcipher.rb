ae70a6ec24b8879dc093835cc006b6fa30469a8a
Fedor <fedor.kis@gmail.com>
2020-01-20 15:18:20 +0100

    Add interface

--- rotcipher.rb
+++ rotcipher.rb
@@ -1,5 +1,9 @@
 class Rot_Cipher
 def cipher (string, shift)
+  puts "This is a caesar cipher. Please put in text to encode"
+  string = gets.chomp
+  puts "Please put in encoding level"
+  shift = gets.chomp.to_i
   alphabet = Array('A'..'Z') + Array('a'..'z')
   rotate = Hash[alphabet.zip(alphabet.rotate(shift))]
   string.chars.map{ |c| rotate.fetch(c)}
@@ -7,4 +11,4 @@ end
 end
 
 test = Rot_Cipher.new
-puts test.cipher("CIPHeR", 1).join
+puts test.cipher("Blank", 1).join
